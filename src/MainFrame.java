db680251a862c68f1a303ae6e8ecea49ffdc4248
hamdysaad20 <hamdysaad.biz@gmail.com>
2022-11-21 00:38:30 +0200

    test

---
+++ src/MainFrame.java
@@ -0,0 +1,64 @@
+import javax.swing.*;
+
+public class MainFrame  extends JFrame{
+    private JLabel usernamelab;
+    private JTextField username_input;
+    private JLabel password;
+    private JButton loginButton;
+    private JButton donTHaveAccountButton;
+    private JPasswordField pass_input;
+    private  JPanel mainPanal;
+    private JPanel container;
+
+
+
+    public  MainFrame(){
+        setContentPane(mainPanal);
+setTitle("test");
+setSize(800,800);
+setDefaultCloseOperation(EXIT_ON_CLOSE);
+setVisible(true);
+    }
+// music_info:arr[] => [music_Price,title,id,artist]
+
+     public static void paymentlogic(double TAX, float music_price, int Quantity){
+     //tax= 2.5%
+       double total_price=(music_price*Quantity)+((music_price*TAX)*Quantity);
+         String[] res = new String[2];
+         res[0]= String.valueOf(total_price);
+         System.out.println(res[0]);
+     }
+
+
+
+
+    public static void main(String[] args)   {
+        final double TAX= 14.5/100;
+        float music_price = (float) 0.99;
+        int Quantity=3;
+        MainFrame frame = new MainFrame();
+        System.out.println("all done");
+
+
+
+
+
+
+
+     }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
